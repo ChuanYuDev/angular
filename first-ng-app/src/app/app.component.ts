7635e05e2ec6e39063c79684681877caf4b6003d
nealmatrix <nealmatrixgg@gmail.com>
2025-07-24 23:38:54 -0700

    Angular component

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
+// Component decorator
+//    Enhances AppComponent class
 @Component({
+  // Indicate the app-root element is actually AppComponent
   selector: 'app-root',
+
   standalone: true,
-  imports: [RouterOutlet],
+
+  // Import component
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+
+  // Template will be rendered inside AppComponent
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <!-- <h1>Welcome to {{title}}!</h1>
+    <p>Hello world!</p> -->
+    <app-header />
+    <main>
+      <app-home />
+    </main>
 
     <router-outlet />
   `,
-  styles: [],
+  // Specify css styles
+  //    
+  styles: [`
+    // When you have a component, Angular adds an attribute to the element
+    //    <app-root _nghost-ng-c3447218235="" ng-version="18.2.13">    
+    //    All the child elements have the same attribute
+    //
+    // Chrome shows
+    //    p[_ngcontent-ng-c3447218235] {
+    //      background-color: red;
+    //    }
+    //    Make sure the styles within the component stays within the component, not leak styles
+    //    If other components have <p>, they are not affected by this style
+    // p {
+    //   background-color: red;
+    // } 
+
+      main {
+        padding: 16px;
+      }
+    `],
 })
 export class AppComponent {
   title = 'first-ng-app';
---
+++ first-ng-app/src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+
+  // Because we change `inlineTemplate` and `inlineStyle` in `angular.json` to false
+  //    We create HTML and scss
+  //    Then we only have templateUrl and styleUrl in the component decorator
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
