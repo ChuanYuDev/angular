32c066aece350fb93ca20b2ff3393878caf15c10
nealmatrix <nealmatrixgg@gmail.com>
2025-08-03 17:15:03 -0700

    4. Event listeners in Angular

--- first-ng-app/src/app/home/home.component.ts
+++ first-ng-app/src/app/home/home.component.ts
@@ -11,4 +11,12 @@ import { GreetingComponent } from '../components/greeting/greeting.component';
 export class HomeComponent {
   // Pass this message to child component GreetingComponent
   homeMessage = signal('Hello, world!');
+
+  // keyUpHandler() {
+  //   console.log('user typed something in the input');
+  // }
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
 }
